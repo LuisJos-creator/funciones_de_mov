a7edef8cc8f438166c33740467f215b7460340ed
LuisJos-creator <69698670+LuisJos-creator@users.noreply.github.com>
2025-03-02 23:05:58 -0400

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,40 +7,12 @@ function Izquierda () {
 function Adelante () {
     motorbit.freestyle(50, 56)
 }
-input.onButtonPressed(Button.A, function () {
-    if (input.buttonIsPressed(Button.A)) {
-        radio.sendNumber(1)
-    } else {
-        radio.sendNumber(0)
-    }
-})
 function Atrás () {
     motorbit.freestyle(-50, -56)
 }
-input.onButtonPressed(Button.AB, function () {
-    if (input.buttonIsPressed(Button.AB)) {
-        radio.sendNumber(3)
-    } else {
-        radio.sendNumber(0)
-    }
-})
 function Derecha () {
     motorbit.freestyle(-50, 56)
 }
-input.onButtonPressed(Button.B, function () {
-    if (input.buttonIsPressed(Button.B)) {
-        radio.sendNumber(2)
-    } else {
-        radio.sendNumber(0)
-    }
-})
-input.onGesture(Gesture.ThreeG, function () {
-    if (input.isGesture(Gesture.ThreeG)) {
-        radio.sendNumber(4)
-    } else {
-        radio.sendNumber(0)
-    }
-})
 let mov = 0
 radio.setGroup(1)
 basic.forever(function () {
@@ -50,11 +22,20 @@ basic.forever(function () {
         Derecha()
     } else if (mov == 3) {
         Adelante()
-    } else if (mov == 4) {
-        motorbit.brake()
     } else if (mov == 0) {
         motorbit.brake()
     } else {
     	
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(1)
+    } else if (input.buttonIsPressed(Button.B)) {
+        radio.sendNumber(2)
+    } else if (input.buttonIsPressed(Button.AB)) {
+        radio.sendNumber(3)
+    } else {
+        radio.sendNumber(0)
     }
 })
